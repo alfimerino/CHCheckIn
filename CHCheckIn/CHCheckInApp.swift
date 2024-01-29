4cadf647fbf57f395ed5f27092a62a33359aad27
Alfredo Merino <alfimerino@gmail.com>
2024-01-29 13:13:13 -0500

    Initial Commit

---
+++ CHCheckIn/CHCheckInApp.swift
@@ -0,0 +1,17 @@
+//
+//  CHCheckInApp.swift
+//  CHCheckIn
+//
+//  Created by Alfredo Merino on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CHCheckInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
